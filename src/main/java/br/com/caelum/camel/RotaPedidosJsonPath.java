07d5248e1fc1bc6d8fff749038f4cca99d0c3a0e
Mariana Barreiros <maridepp1@hotmail.com>
2019-03-31 13:20:23 -0300

    Add RotaPedidosJsonPath class that read values from a json file

--- src/main/java/br/com/caelum/camel/RotaPedidosJsonPath.java
+++ src/main/java/br/com/caelum/camel/RotaPedidosJsonPath.java
@@ -4,8 +4,7 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class RotaPedidosXPath {
-	
+public class RotaPedidosJsonPath {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
@@ -14,12 +13,16 @@ public class RotaPedidosXPath {
 				@Override
 				public void configure() throws Exception {
 					//noop=true faz com que os arquivos continuem em pedidos
-					from("file:pedidos?delay=5s&noop=true").
-						split().
-							xpath("pedido/itens/item").
-						filter().
-							xpath("item/formato[text()='EBOOK']").
-						log("${id} - ${body}").
+					from("file:pedidosJson?delay=5s&noop=true").
+					//Foi necessário adicionar o jsonpath no pom.xml.
+						setProperty("enabled").jsonpath("$.errorHandling.enabled").
+						setProperty("httpErrorCode").jsonpath("$.errorHandling.errorCodes[*].httpErrorCode").
+						setProperty("business").jsonpath("$.errorHandling.errorCodes[*].business[*].enabled").
+						setProperty("retry").jsonpath("$.errorHandling.errorCodes[*].business[*].retry.enabled").
+						log("Error Hadling: ${property.enabled}").
+						log("HttpErrorCode: ${property.httpErrorCode}").
+						log("business: ${property.business}").
+						log("retry: ${property.retry}").
 						//noext faz com que o arquivo não fique com a extensão xml
 						//setHeader("camelFileName", simple("${file:name.noext}.json")).
 					to("file:saida");
@@ -30,5 +33,4 @@ public class RotaPedidosXPath {
 		Thread.sleep(20000);
 		context.stop();
 		}
-
 }
